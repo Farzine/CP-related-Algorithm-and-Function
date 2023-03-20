d68a3a40db2ed3d582e02d60668e84efb606cbaf
Farzine <alfezafarzine@gmail.com>
2023-03-20 16:02:24 +0600

    Create

---
+++ some build in function.cpp
@@ -0,0 +1,17 @@
+
+// GCD
+
+/*Library: 'algorithm'
+ __gcd(m, n) 
+Parameter :  m, n
+Return Value :  0 if both m and n are zero, 
+else gcd of m and n.*/
+
+#include <iostream>
+#include <algorithm>
+// #include<numeric> for C++17
+using namespace std;
+int main()
+{
+	cout << "gcd(6, 20) = " << __gcd(6, 20) << endl; // gcd(6,20) for C++17
+}
