29ba86d47312b9020d3c38d1529540718ba348a3
Farzine <alfezafarzine@gmail.com>
2023-03-27 15:32:53 +0600

    Update some build in function.cpp

--- some build in function.cpp
+++ some build in function.cpp
@@ -15,3 +15,28 @@ int main()
 {
 	cout << "gcd(6, 20) = " << __gcd(6, 20) << endl; // gcd(6,20) for C++17
 }
+
+
+//Find max element in an array
+// C++ program to find the max
+// of Array using *max_element() in STL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// Get the vector
+	vector<int> a = { 1, 45, 54, 71, 76, 12 };
+
+	// Print the vector
+	cout << "Vector: ";
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << " ";
+	cout << endl;
+
+	// Find the max element
+	cout << "\nMax Element = "
+		<< *max_element(a.begin(), a.end());
+	return 0;
+}
