e3c6d2e2fd1991dfab7fb34d65199f3fa779bc7c
Farzine <alfezafarzine@gmail.com>
2023-06-06 11:24:32 +0600

    Update some build in function.cpp

--- some build in function.cpp
+++ some build in function.cpp
@@ -218,5 +218,25 @@ printf("a<<1 = %d\n", (a << 1));
 printf("b<<1 = %d", (b << 1));
 return 0;
 }
-///////////////////////////////////////////
+////////////////////////////////////////
+															    
+// Right shift operator
+// use of right-shift operator---->> a/2 <--formula
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+// a = 5(00000101), b = 9(00001001)
+unsigned char a = 20, b = 30;
+
+// The result is 00000010
+printf("a>>1 = %d\n", (a >> 1));
+
+// The result is 00000100
+printf("b>>1 = %d", (b >> 1));
+
+return 0;
+}
+///////////////////////////////////////
 															   
