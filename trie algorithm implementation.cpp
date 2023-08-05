935585397d3947dcfbe1765e237fc6b0fe5ff673
Farzine <alfezafarzine@gmail.com>
2023-08-06 00:58:22 +0600

    Create trie algorithm implementation.cpp

---
+++ trie algorithm implementation.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Trie{
+    public:
+        class node{
+            public:
+            bool end;
+            node* next[26];
+            node(){
+                end = false;
+                for(int i=0; i<26; i++)
+                {
+                    next[i] = NULL;
+                }
+            }
+            
+        };
+        node* trie;
+        Trie(){
+            trie = new node();
+        }
+
+        void insert(string word){
+            int i = 0;
+            node* it = trie;
+            while(i < word.size()){
+                if(it -> next[word[i] - 'a'] == NULL)
+                it -> next[word[i] - 'a'] = new node();
+                it = it -> next[word[i] - 'a'];
+                i++;
+            }
+            it ->end = true;
+        }
+
+    bool search(string word){
+          int i = 0;
+          node* it = trie;
+          while(i < word.size()){
+            if(it -> next[word[i] - 'a'] == NULL)
+            return false;
+            it = it -> next[word[i] - 'a'];
+            i++;
+          }
+          return it ->end;
+    }
+};
+
+int main()
+{
+    Trie *myTrie = new Trie();
+    vector<string> words = {"ayush", "Farzine"};
+    for(auto w: words)
+    {
+        myTrie -> insert(w);
+    }
+    if(myTrie -> search("Farzine"))
+    {
+        cout<<"Farzine found"<<endl;
+    }
+    else cout<<"Farzine not found"<<endl;
+    if(myTrie -> search("madhav"))
+    {
+        cout<<"madhav found"<<endl;
+    }
+    else cout<<"madhav not found"<<endl;
+}
