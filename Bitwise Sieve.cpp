c5b0a5ea19b924b52cddca03b054a9673d18c1c2
Farzine <alfezafarzine@gmail.com>
2024-01-23 19:13:40 +0600

    Update Bitwise Sieve.cpp

--- Bitwise Sieve.cpp
+++ Bitwise Sieve.cpp
@@ -1,64 +1,43 @@
-// C++ program to implement bitwise Sieve
-// of Eratosthenes.
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Checks whether x is prime or composite
-bool ifnotPrime(int prime[], int x)
+
+int N =100,prime[100]; 
+int status[100/32];
+
+bool check(int N,int pos){return (bool)(N & (1<<pos));}
+int Set(int N,int pos){	return N=N | (1<<pos);}
+
+void sieve()
 {
-	// checking whether the value of element
-	// is set or not. Using prime[x/64], we find
-	// the slot in prime array. To find the bit
-	// number, we divide x by 2 and take its mod
-	// with 32.
-	return (prime[x/64] & (1 << ((x >> 1) & 31)));
+     int i, j, sqrtN; 
+     sqrtN = int( sqrt( N ) );
+     for( i = 3; i <= sqrtN; i += 2 ) 
+     {
+		 if( check(status[i/32],i%32)==0)
+		 {
+	 		 for( j = i*i; j <= N; j += 2*i )
+			 {
+				 status[j/32]=Set(status[j/32],j % 32)   ;
+	 		 }
+		 }
+	 }
+	 puts("2");
+	 for(i=3;i<=N;i+=2)
+		 if( check(status[i/32],i%32)==0)
+	 	 printf("%d\n",i);
+	 	
 }
 
-// Marks x composite in prime[]
-bool makeComposite(int prime[], int x)
+
+signed main()
 {
-	// Set a bit corresponding to given element.
-	// Using prime[x/64], we find the slot in prime
-	// array. To find the bit number, we divide x
-	// by 2 and take its mod with 32.
-	prime[x/64] |= (1 << ((x >> 1) & 31));
-}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
 
-// Prints all prime numbers smaller than n.
-void bitWiseSieve(int n)
-{
-	// Assuming that n takes 32 bits, we reduce
-	// size to n/64 from n/2.
-	int prime[n/64];
+    
+    sieve();
 
-	// Initializing values to 0 .
-	memset(prime, 0, sizeof(prime));
 
-	// 2 is the only even prime so we can ignore that
-	// loop starts from 3 as we have used in sieve of
-	// Eratosthenes .
-	for (int i = 3; i * i <= n; i += 2) {
-
-		// If i is prime, mark all its multiples as
-		// composite
-		if (!ifnotPrime(prime, i))
-			for (int j = i * i, k = i << 1; j < n; j += k)
-				makeComposite(prime, j);
-	}
-
-	// writing 2 separately
-	printf("2 ");
-
-	// Printing other primes
-	for (int i = 3; i <= n; i += 2)
-		if (!ifnotPrime(prime, i))
-			printf("%d ", i);
-}
-
-// Driver code
-int main()
-{
-	int n = 100;
-	bitWiseSieve(n);
-	return 0;
+    return 0;
 }
